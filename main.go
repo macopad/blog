0bd70e659e36bf089b32471dfff0bd922a1df6af
macopad <macopad@qq.com>
2021-09-03 16:11:23 +0800

    add

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	engine.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(200, "hello world")
+	})
+
+	engine.Run()
+}
