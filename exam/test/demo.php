6f4e29c5271df91cba6d2b6d340d36fa45f5967f
macopad <macopad@qq.com>
2022-05-21 14:34:47 +0800

    update code for water problem

--- exam/test/demo.php
+++ exam/test/demo.php
@@ -527,9 +527,28 @@ function twonumsaddforptr($x, $y) {
 }
 
 //容器最多的雨水/接雨水问题
-function waterproblem() {
+function waterproblem($height = array()) {
+    $left = 0;
+    $right = count($height) - 1;
+    $ans = 0;
     
+    $left_max = 0;
+    $right_max = 0;
+    
+    while ($left < $right) {
+        if($height[$left] < $height[$right]) {
+            ($height[$left] > $left_max)? $left_max = $height[$left] : $ans += $left_max - $height[$left];
+            $left++;
+        } else {
+            ($height[$right] > $right_max) ? $right_max = $height[$right] : $ans += $right_max - $height[$right];
+            $right--;
+        }
+    }
+    return $ans;
 }
+//$height = array(1,2,0,0,0,0,9);
+//print_r(waterproblem($height));
+
 
 //删除链表第N个节点
 function deleteListN($list , $num) {
